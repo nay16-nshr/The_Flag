0f992f9f3b56449e8291f15b863e35f93e34ff41
jbt <yabs1002@gmail.com>
2025-08-25 16:49:54 +0300

    add files

--- Screen.py
+++ Screen.py
@@ -1,23 +1,24 @@
 import pygame
+import  game_filed
+from main import screen, color_screen
 
-# Initialize Pygame
 pygame.init()
 
-# Set up the game window
-screen = pygame.display.set_mode((1000,500))#each pixel represent 1:20
-color_screen = (0, 200, 0)
-screen.fill(color_screen)
-pygame.display.set_caption("The_Flag")
-# Game loop
-running = True
-while running:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    pygame.display.update()
-# Quit Pygame
-pygame.quit()
+
+
+
+
+def draw_soldier():
+    pass
+
+
+def draw_grass():
+    pass
+
+
 def draw_game():
     screen.fill(color_screen)
+    game_filed.draw()
+    draw_soldier()
     draw_grass()
---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+pygame.init()
+color_screen = (0, 200, 0)
+screen = pygame.display.set_mode((1000,500))#each pixel represent 1:20
+pygame.display.set_caption("The_Flag")
+# Game loop
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
+# Quit Pygame
+pygame.quit()
