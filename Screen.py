d43ae9602de0b0634bc544aa179875e6c0c1beda
jbt <yabs1002@gmail.com>
2025-08-25 16:58:09 +0300

    add files

--- Screen.py
+++ Screen.py
@@ -17,8 +17,13 @@ def draw_grass():
     pass
 
 
+def draw_flag():
+    pass
+
+
 def draw_game():
     screen.fill(color_screen)
     game_filed.draw()
     draw_soldier()
     draw_grass()
+    draw_flag()
