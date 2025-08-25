3ced80265d7034bbb43bdd17791e64c7563f1b90
jbt <naominesher16@gmail.com>
2025-08-25 16:52:59 +0300

    add main

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+
+state = {  "is_window_open": True,
+
+}
+def main():
+    pygame.init()
+    while  state["is_window_open"]:
+        handle_user_events()
+
+def handle_user_events():
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
