80beb06339a3ca836b64de6893787e7790b170f0
jbt <yabs1002@gmail.com>
2025-08-26 15:40:39 +0300

    add files

--- Screen.py
+++ Screen.py
@@ -9,35 +9,33 @@ pygame.display.set_caption("The_Flag")
 def draw_soldier(soldier):
     screen.blit(soldier["img"],change_pos_field_to_pos(soldier["pos"]))
 
-
-
-def draw_grass(grass):
-    screen.blit(grass["img"], change_pos_field_to_pos(grass["pos"]))
-
-
+def draw_grass(pos,grass_img):
+    screen.blit(grass_img, change_pos_field_to_pos(pos))
 
 def draw_flag(flag):
     screen.blit(flag["img"], change_pos_field_to_pos(flag["pos"]))
 def draw_mine(pos):
     mine_img = pygame.image.load("mine.png")
     mine_img = pygame.transform.scale(mine_img, (Const.MINE_WIDTH, Const.MINE_HEIGHT))
-
     screen.blit(mine_img, change_pos_field_to_pos(pos))
 
-def draw(soldier,flag,game_field):
+def draw(soldier,flag):
     screen.fill(Const.COLOR_SCREEN)
     draw_soldier(soldier)
     draw_flag(flag)
-    for i in range(Const.GRASS_NUMBER):
-                draw_grass(Game_field.create_grass())
+    dick_grass = Game_field.create_grass()
+    for pos in dick_grass["list_of_pos_grass"]:
+        draw_grass(pos,dick_grass["img"])
 
 
 
 def draw_dark_mode_game(soldier, game_field):
     screen.fill(Const.COLOR_DARK_SCREEN)
 
-    for i in range (Const.TILE_SIZE,Const.SCREEN_WIDTH,Const.TILE_SIZE):
-        pygame.draw.line(screen,Const.LINE_COLOR,(i,0),(i,Const.SCREEN_HEIGHT))
+    for x in range (Const.TILE_SIZE,Const.SCREEN_HEIGHT,Const.TILE_SIZE):
+        pygame.draw.line(screen,Const.LINE_COLOR,(x,0),(x,Const.SCREEN_WIDTH))
+    for y in range (Const.TILE_SIZE,Const.SCREEN_WIDTH,Const.TILE_SIZE):
+        pygame.draw.line(screen,Const.LINE_COLOR,(0,y),(Const.SCREEN_HEIGHT,y))
     for i in range(Const.MINE_NUMBER):
         for row in range(len(game_field)):
             for col in range(len(game_field[row])):
@@ -50,9 +48,11 @@ def draw_dark_mode_game(soldier, game_field):
 def draw_game(state):
     soldier = state["soldier"]
     flag = Game_field.create_flag()
-    screen.fill(Const.COLOR_SCREEN)
-    draw(soldier,flag,Game_field.game_field)
+    pygame.display.flip()
+    draw(soldier,flag)
+    pygame.display.flip()
     if state["is_enter"]:
         draw_dark_mode_game(soldier, Game_field.game_field)
+    pygame.display.flip()
 def change_pos_field_to_pos(pos):
     return (pos[0]*20,pos[1]*20)
