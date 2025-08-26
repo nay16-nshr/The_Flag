eb5c88ea5ca1b0d9c15ca050b964583e73347fde
Owner <yabs1002@gmail.com>
2025-08-26 07:34:53 +0300

    change

--- game_field.py
+++ game_field.py
@@ -1,4 +1,8 @@
-def creat():
+def create_game_field():
+    pass
+def create_flag():
+    pass
+def create_mine():
     pass
 def draw():
     pass
--- soldier.py
+++ soldier.py
@@ -1,6 +1,14 @@
-def creat():
+def create():
     pass
 def move_in_direction():
     pass
 def should_stop():
+    pass
+def position_top_left():
+    pass
+def position_body():
+    pass
+def position_legs():
+    pass
+def is_in_game_field():
     pass
