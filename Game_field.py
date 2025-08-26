7e9746de90f96406dd2a88a0fe14cd50c550e6e7
jbt <naominesher16@gmail.com>
2025-08-26 11:35:44 +0300

    hi

--- Game_field.py
+++ Game_field.py
@@ -1,8 +1,30 @@
 import Const
+import Screen
+from main import screen
 import pygame
 import random
+game_field = []
+
+def create_empty_field():
+    for row in range(Const.SIZE_ROW):
+        game_field.append(["empty"]*Const.SIZE_COL)
+
+
 def create_game_field():
-    pass
+    global game_field
+    create_empty_field()
+    game_field[Const.FLAG_POS_FIELD]
+    count_mine = 0
+    while count_mine < Const.MINE_NUMBER:
+        mine_index = (random.randint(0, 22),random.randint(0,49))
+        is_empty = True
+        for i in range(3):
+            if game_field[mine_index[0]+i][mine_index[1]] != "empty":
+                is_empty=False
+        if is_empty:
+            for i in range(3):
+                game_field[mine_index[0] + i][mine_index[1]] = "mine"
+            count_mine += 1
 
 
 def create_flag():
