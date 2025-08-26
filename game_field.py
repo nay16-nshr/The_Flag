ee1f9b731cf23e4f806c009ba8bffd6388ee94bb
jbt <yabs1002@gmail.com>
2025-08-26 09:58:20 +0300

    add files

--- game_field.py
+++ game_field.py
@@ -1,16 +1,28 @@
 import Const
-
+import Screen
+from main import screen
+import pygame
 
 def create_game_field():
     global game_field
+    count_mine = 0
     for row in range(Const.SIZE_ROW):
         for col in range(Const.SIZE_COL):
-            pass
+            while count_mine < Const.MINE_NUMBER:
+                pass
+
+
 
     pass
 def create_flag():
     pass
 def create_mine():
     pass
-def draw():
+def draw(soldier_img,flag_img,grass_img):
+    screen.fill(Const.color_screen)
+    Screen.draw_soldier(soldier_img)
+    Screen.draw_flag(flag_img)
+    for i in range(Const.GRASS_NUMBER):
+        if :
+            Screen.draw_grass(grass_img)
     pass
